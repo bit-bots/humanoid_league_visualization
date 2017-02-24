d22473c9cb4627e71119cd2a6dc74174dff15d2c
Marc <0bestman@informatik.uni-hamburg.de>
2017-02-24 14:49:47 +0100

    fixed bugs and added locks against threading problems. Showing of ball candidates does now work

--- humanoid_league_vision_viz/src/humanoid_league_vision_viz/vision_viz.py
+++ humanoid_league_vision_viz/src/humanoid_league_vision_viz/vision_viz.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2.7
 import copy
+import threading
 from collections import OrderedDict
 
 import cv2
@@ -21,7 +22,9 @@ def draw_ball(cv_img, ball):
 
 
 def draw_ball_candidates(cv_img, candidates):
+    rospy.logwarn(candidates)
     if len(candidates) > 0:
+        rospy.logwarn("drawing")
         for can in candidates:
             i = [0, 0, 0]
             i[0] = int(can.center.x)
@@ -41,12 +44,15 @@ def draw_ball_candidates(cv_img, candidates):
 
 class VisionViz:
     def __init__(self):
-        rospy.init_node("bitbots_imageviewer")
+        rospy.init_node("bitbots_vision_viz")
 
         self.bridge = CvBridge()
         self.images = OrderedDict()
         self.ball_candidates = OrderedDict()
         self.balls = OrderedDict()
+
+        self.img_lock = threading.Lock()
+        self.can_lock = threading.Lock()
 
         #todo these have to be dyn reconfigurable
         self.candidates_active = True
@@ -58,36 +64,47 @@ class VisionViz:
         rospy.Subscriber("/ball_candidates", BallsInImage, self._candidates_cb, queue_size=10)
 
         self.run()
-        rospy.spin()
 
     def run(self):
+        rate = rospy.Rate(100)
         while not rospy.is_shutdown():
-            images = copy.deepcopy(self.images)
+            with self.img_lock:
+                images = copy.deepcopy(self.images)
+
             # print("Waiting for " + str(self.images.keys()))
             for t in images.keys():  # imgages who are wating
+                img = images.pop(t)  # get image from queue
+                cv_img = self.bridge.imgmsg_to_cv2(img, "bgr8")
+                with self.can_lock:
+                    if t in self.ball_candidates:  # Check if all data to draw is there
+                        candidates = self.ball_candidates.pop(t)
+                        #ball = self.balls.pop(t)
 
-                if t in self.ball_candidates:  # Check if all data to draw is there
+                        if self.candidates_active:
+                            rospy.logwarn("1")
+                            draw_ball_candidates(cv_img, candidates)
+                        #if self.ball_active:
+                        #    draw_ball(cv_img, ball)
 
-                    img = images.pop(t)  # get image from queue
-                    candidates = self.ball_candidates.pop(t)
-                    ball = self.balls.pop(t)
-                    cv_img = self.bridge.imgmsg_to_cv2(img, "bgr8")
-
-                    if self.candidates_active:
-                        draw_ball_candidates(cv_img, candidates)
-                    if self.ball_active:
-                        draw_ball(cv_img, ball)
-
-                    out_msg = self.bridge.cv2_to_compressed_imgmsg(cv_img)
-                    self.viz_publisher.publish(out_msg)
+                        out_msg = self.bridge.cv2_to_imgmsg(cv_img, encoding="bgr8")
+                        self.viz_publisher.publish(out_msg)
+            rate.sleep()
 
     def _image_cb(self, msg):
-        self.images[msg.header.stamp] = msg
+        with self.img_lock:
+            self.images[msg.header.stamp] = msg
 
-        if len(self.images) >= 10:
-            self.images.popitem(last=False)
+            if len(self.images) >= 10:
+                self.images.popitem(last=False)
 
     def _candidates_cb(self, msg):
-        self.ball_candidates[msg.header.stamp] = msg.candidates
-        if len(self.ball_candidates) > 5:
-            self.ball_candidates.popitem(last=False)
+        with self.can_lock:
+            rospy.logwarn("cans")
+
+            self.ball_candidates[msg.header.stamp] = msg.candidates
+            if len(self.ball_candidates) > 5:
+                self.ball_candidates.popitem(last=False)
+
+
+if __name__ == "__main__":
+    cm730_node = VisionViz()
