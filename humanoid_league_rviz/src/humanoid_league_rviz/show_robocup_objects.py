970d9694f9832400070772bd47d05c138e1ce5a9
Marc <0bestman@informatik.uni-hamburg.de>
2017-02-24 18:29:46 +0100

    added markings for goal posts and obstacles

--- humanoid_league_rviz/src/humanoid_league_rviz/show_robocup_objects.py
+++ humanoid_league_rviz/src/humanoid_league_rviz/show_robocup_objects.py
@@ -1,36 +1,96 @@
+#!/usr/bin/env python3
+
 import rospy
 import time
 
 from geometry_msgs.msg import Pose, Vector3
 from std_msgs.msg import ColorRGBA
 from visualization_msgs.msg import MarkerArray, Marker
-from humanoid_league_msgs.msg import BallRelative
+from humanoid_league_msgs.msg import BallRelative, GoalRelative, ObstaclesRelative
 
 
 class ShowRobocupObjects:
     def __init__(self):
+        rospy.init_node("show_robocup_objects")
+        #todo make dyn reconf able
         self.marker_publisher = rospy.Publisher("/robocup_markers", Marker, queue_size=10)
 
-        # initilize message objects
+
+        # object properties
+        self.ball_diameter = 0.13
+        self.ball_lifetime = int(0.1*(10**9))
+        self.post_diameter = 0.10
+        self.post_height = 1.10
+        self.goal_lifetime = int(0.1*(10**9))
+        self.obstacle_height = 1.0
+        self.obstacle_lifetime = int(1*(10**9))
+        self.obstacle_def_width = 0.3
+
+        # --- initilize message objects ---
+        self.frame_id = "base_link"
+        # ball
         self.marker_ball_rel = Marker()  # type: Marker
-        self.marker_ball_rel.header.frame_id = "base_link"
+        self.marker_ball_rel.header.frame_id = self.frame_id
         self.marker_ball_rel.ns = "rel_ball"
         self.marker_ball_rel.id = 0
         self.marker_ball_rel.type = Marker.SPHERE
         self.marker_ball_rel.action = Marker.MODIFY
         self.ball_pose = Pose()
-        scale = Vector3(1, 1, 1)
+        scale = Vector3(self.ball_diameter, self.ball_diameter, self.ball_diameter)
         self.marker_ball_rel.scale = scale
-        color = ColorRGBA()
-        color.r = 1.0
-        color.a = 1.0
-        self.marker_ball_rel.color = color
-        self.marker_ball_rel.lifetime = 1
+        self.ball_color = ColorRGBA()
+        self.ball_color.r = 1.0
+        self.ball_color.a = 1.0
+        self.marker_ball_rel.color = self.ball_color
+        self.marker_ball_rel.lifetime = rospy.Duration(nsecs=self.ball_lifetime)
+        #goal
+        ## post 1
+        self.marker_goal_rel1 = Marker() # type:Marker
+        self.marker_goal_rel1.header.frame_id = self.frame_id
+        self.marker_goal_rel1.ns = "rel_goal"
+        self.marker_goal_rel1.id = 0
+        self.marker_goal_rel1.type = Marker.CYLINDER
+        self.marker_goal_rel1.action = Marker.MODIFY
+        self.goal_post1_pose = Pose()
+        scale = Vector3(self.post_diameter, self.post_diameter, self.post_height)
+        self.marker_goal_rel1.scale = scale
+        self.post1_color = ColorRGBA()
+        self.post1_color.r = 1.0
+        self.post1_color.g = 1.0
+        self.post1_color.b = 1.0
+        self.post1_color.a = 1.0
+        self.marker_goal_rel1.color = self.post1_color
+        self.marker_goal_rel1.lifetime = rospy.Duration(nsecs=self.goal_lifetime)
+        ## post 2
+        self.marker_goal_rel2 = Marker() # type:Marker
+        self.marker_goal_rel2.header.frame_id = self.frame_id
+        self.marker_goal_rel2.ns = "rel_goal"
+        self.marker_goal_rel2.id = 1
+        self.marker_goal_rel2.type = Marker.CYLINDER
+        self.marker_goal_rel2.action = Marker.MODIFY
+        self.goal_post2_pose = Pose()
+        scale = Vector3(self.post_diameter, self.post_diameter, self.post_height)
+        self.marker_goal_rel2.scale = scale
+        self.post2_color = ColorRGBA()
+        self.post2_color.r = 1.0
+        self.post2_color.g = 1.0
+        self.post2_color.b = 1.0
+        self.post2_color.a = 1.0
+        self.marker_goal_rel2.color = self.post2_color
+        self.marker_goal_rel2.lifetime = rospy.Duration(nsecs=self.goal_lifetime)
+        # obstacle
+        self.marker_obstacle = Marker()
+        self.marker_obstacle.lifetime = rospy.Duration(nsecs=self.obstacle_lifetime)
+        self.marker_obstacle.ns = "rel_obstacle"
+        self.obstacle_color = ColorRGBA()
+        self.obstacle_pose = Pose()
+        self.marker_obstacle.type = Marker.CUBE
+        self.marker_obstacle.header.frame_id = self.frame_id
 
         # todo also display data from world model
-        rospy.Subscriber("/ball_relative", self.ball_cb, queue_size=10)
-        #rospy.Subscriber("/goal_relative", self.goal_cb, queue_size=10)
-        #rospy.Subscriber("/obstacles_relative", self.obstacle_cb, queue_size=10)
+        rospy.Subscriber("/ball_relative", BallRelative, self.ball_cb, queue_size=10)
+        rospy.Subscriber("/goal_relative", GoalRelative, self.goal_cb, queue_size=10)
+        rospy.Subscriber("/obstacles_relative", ObstaclesRelative, self.obstacle_cb, queue_size=10)
 
         # we do everything in the callbacks
         rospy.spin()
@@ -39,4 +99,71 @@ class ShowRobocupObjects:
         self.marker_ball_rel.header.stamp = rospy.Time.from_sec(time.time())
 
         self.ball_pose.position = msg.ball_relative
+        self.ball_pose.position.z = self.ball_diameter/2
         self.marker_ball_rel.pose = self.ball_pose
+        self.ball_color.a = msg.confidence
+        self.marker_ball_rel.color = self.ball_color
+
+        self.marker_publisher.publish(self.marker_ball_rel)
+
+    def goal_cb(self, msg: GoalRelative):
+        if len(msg.positions) > 0:
+            self.marker_goal_rel1.header.stamp = rospy.Time.from_sec(time.time())
+            self.goal_post1_pose.position = msg.positions[0]
+            self.goal_post1_pose.position.z = self.post_height/2
+            self.marker_goal_rel1.pose = self.goal_post1_pose
+            self.post1_color.a = msg.confidence
+            self.marker_goal_rel1.color = self.post1_color
+            self.marker_publisher.publish(self.marker_goal_rel1)
+
+        if len(msg.positions) > 1:
+            self.marker_goal_rel2.header.stamp = rospy.Time.from_sec(time.time())
+            self.goal_post2_pose.position = msg.positions[0]
+            self.goal_post2_pose.position.z = self.post_height/2
+            self.marker_goal_rel1.pose = self.goal_post2_pose
+            self.post2_color.a = msg.confidence
+            self.marker_goal_rel2.color = self.post2_color
+            self.marker_publisher.publish(self.marker_goal_rel2)
+
+    def obstacle_cb(self, msg: ObstaclesRelative):
+        i = 0
+        for obstacle in msg.obstacles:
+            self.marker_obstacle.header.stamp = rospy.Time.from_sec(time.time())
+            self.marker_obstacle.id = i
+            i += 1
+            self.obstacle_color.a = obstacle.confidence
+            # color depding on type of obstacle
+            if obstacle.type == obstacle.CYAN_ROBOT:
+                self.obstacle_color.r = 0.0
+                self.obstacle_color.g = 0.0
+                self.obstacle_color.b = 1.0
+            elif obstacle.type == obstacle.MAGENTA_ROBOT:
+                self.obstacle_color.r = 1.0
+                self.obstacle_color.g = 0.0
+                self.obstacle_color.b = 0.0
+            elif obstacle.type == obstacle.UNKNOWN_ROBOT:
+                self.obstacle_color.r = 0.0
+                self.obstacle_color.g = 1.0
+                self.obstacle_color.b = 0.0
+            else:
+                self.obstacle_color.r = 0.5
+                self.obstacle_color.g = 0.5
+                self.obstacle_color.b = 0.5
+            self.marker_obstacle.color = self.obstacle_color
+
+            # size if provided
+            if obstacle.width > 0.0:
+                scale = Vector3(obstacle.width, obstacle.width, self.obstacle_height)
+            else:
+                scale = Vector3(self.obstacle_def_width, self.obstacle_def_width, self.obstacle_height)
+            self.marker_obstacle.scale = scale
+
+            # position
+            self.obstacle_pose.position = obstacle.position
+            self.obstacle_pose.position.z = self.obstacle_height / 2
+            self.marker_obstacle.pose = self.obstacle_pose
+
+            self.marker_publisher.publish(self.marker_obstacle)
+
+if __name__ == "__main__":
+    cm730_node = ShowRobocupObjects()
