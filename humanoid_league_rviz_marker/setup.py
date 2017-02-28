16f790106a6e906ef1f0edfa2a34931d9deb60c7
Marc <0bestman@informatik.uni-hamburg.de>
2017-02-28 14:09:23 +0100

    refactored name of hl_rviz to hl_rviz_marker

--- humanoid_league_rviz_marker/setup.py
+++ humanoid_league_rviz_marker/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['humanoid_league_rviz'],
+    packages=['humanoid_league_rviz_marker'],
     #scripts=['bin/myscript'],
     package_dir={'': 'src'}
 )
