b6e28cf02a13e14db2cc6e4c7eb7641373ae8524
Jan Gutsche <7gutsche@informatik.uni-hamburg.de>
2020-08-18 11:06:50 +0000

    Update fkate_vision to new messages

--- humanoid_league_gazebo_world/scripts/fake_vision.py
+++ humanoid_league_gazebo_world/scripts/fake_vision.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-from gazebo_msgs.msg import ModelStates, ModelState
-import tf2_ros
 import rospy
+import tf2_ros
+import numpy as np
+from copy import deepcopy
 from sensor_msgs.msg import CameraInfo
 from tf2_geometry_msgs import PoseStamped
-import numpy as np
-from humanoid_league_msgs.msg import BallRelative, GoalRelative
-from copy import deepcopy
+from gazebo_msgs.msg import ModelStates, ModelState
+from humanoid_league_msgs.msg import PoseWithCertainty, PoseWithCertaintyArray
+from humanoid_league_msgs.msg import GoalRelative
 
 rospy.init_node("fake_vis_in_sim")
 
@@ -34,11 +35,10 @@ def state_update(state_msg):
     if not cam_info:
         return
 
-    ball_index = 0
-    for name in state_msg.name:
+    ball_indices = []
+    for i, name in enumerate(state_msg.name):
         if name == "teensize_ball":
-            break
-        ball_index += 1
+            ball_indices.append(i)
 
 
     goal_index1 = 0
@@ -51,35 +51,43 @@ def state_update(state_msg):
                 break
         goal_index2 += 1
 
-    ball_pose = state_msg.pose[ball_index]
-    goal1_pose = state_msg.pose[goal_index1]
-    goal2_pose = state_msg.pose[goal_index2]
+    ball_msgs = []
+    for ball_index in ball_indices:
+        ball_pose = state_msg.pose[ball_index]
+        goal1_pose = state_msg.pose[goal_index1]
+        goal2_pose = state_msg.pose[goal_index2]
 
-    ball_pose_stamped = PoseStamped()
-    ball_pose_stamped.header.stamp = rospy.Time.now()
-    ball_pose_stamped.header.frame_id = "map"
-    ball_pose_stamped.pose = ball_pose
-    ball_in_camera_optical_frame = tf_buffer.transform(ball_pose_stamped, cam_info.header.frame_id, timeout=rospy.Duration(0.5))
+        ball_pose_stamped = PoseStamped()
+        ball_pose_stamped.header.stamp = rospy.Time.now()
+        ball_pose_stamped.header.frame_id = "map"
+        ball_pose_stamped.pose = ball_pose
+        ball_in_camera_optical_frame = tf_buffer.transform(ball_pose_stamped, cam_info.header.frame_id, timeout=rospy.Duration(0.5))
 
-    # we only have to compute if the ball is inside the image, if the ball is in front of the camera
-    if ball_in_camera_optical_frame.pose.position.z >= 0:
-        p = [ball_in_camera_optical_frame.pose.position.x, ball_in_camera_optical_frame.pose.position.y, ball_in_camera_optical_frame.pose.position.z]
-        k = np.reshape(cam_info.K, (3,3))
-        p_pixel = np.matmul(k, p)
-        p_pixel = p_pixel * (1/p_pixel[2])
+        # we only have to compute if the ball is inside the image, if the ball is in front of the camera
+        if ball_in_camera_optical_frame.pose.position.z >= 0:
+            p = [ball_in_camera_optical_frame.pose.position.x, ball_in_camera_optical_frame.pose.position.y, ball_in_camera_optical_frame.pose.position.z]
+            k = np.reshape(cam_info.K, (3,3))
+            p_pixel = np.matmul(k, p)
+            p_pixel = p_pixel * (1/p_pixel[2])
 
-        # make sure that the transformed pixel is inside the resolution and positive.
-        # also z has to be positive to make sure that the ball is in front of the camera and not in the back
-        if 0 < p_pixel[0] <= cam_info.width and 0 < p_pixel[1] <= cam_info.height:
-            ball_relative = BallRelative()
-            ball_relative.header.stamp = ball_in_camera_optical_frame.header.stamp
-            ball_relative.header.frame_id = "base_footprint"
-            ball_in_footprint_frame = tf_buffer.transform(ball_in_camera_optical_frame, "base_footprint",
-                                                    timeout=rospy.Duration(0.5))
-            ball_relative.header = ball_in_footprint_frame.header
-            ball_relative.ball_relative = ball_in_footprint_frame.pose.position
-            ball_relative.confidence = 1.0
-            ball_pub.publish(ball_relative)
+            # make sure that the transformed pixel is inside the resolution and positive.
+            # also z has to be positive to make sure that the ball is in front of the camera and not in the back
+            if 0 < p_pixel[0] <= cam_info.width and 0 < p_pixel[1] <= cam_info.height:
+                ball_in_footprint_frame = tf_buffer.transform(
+                    ball_in_camera_optical_frame,
+                    "base_footprint",
+                    timeout=rospy.Duration(0.5))
+
+                ball_relative = PoseWithCertainty()
+                ball_relative.pose.pose = ball_in_footprint_frame.pose.position
+                ball_relative.confidence = 1.0
+                ball_msgs.append(ball_relative)
+
+            balls_relative_msg = PoseWithCertaintyArray()
+            balls_relative_msg.header = ball_in_footprint_frame.header
+            balls_relative_msg.poses = ball_msgs
+            ball_pub.publish(balls_relative_msg)
+
 
     for gp in (goal1_pose, goal2_pose):
         goal_pose_stamped = PoseStamped()
@@ -142,7 +150,7 @@ if __name__ == "__main__":
     tf_buffer.can_transform("base_footprint", "camera_optical_frame", rospy.Time(0), timeout=rospy.Duration(30))
     tf_buffer.can_transform("map", "camera_optical_frame", rospy.Time(0), timeout=rospy.Duration(30))
 
-    ball_pub = rospy.Publisher("/ball_relative", BallRelative, queue_size=1)
+    balls_pub = rospy.Publisher("/balls_relative", PoseWithCertaintyArray, queue_size=1)
     goal_pub = rospy.Publisher("/goal_relative", GoalRelative, queue_size=1)
     model_subscriber = rospy.Subscriber("/gazebo/model_states", ModelStates, state_update, tcp_nodelay=True, queue_size=1)
     cam_info_sub = rospy.Subscriber("/camera_info", CameraInfo, cam_info_cb)
