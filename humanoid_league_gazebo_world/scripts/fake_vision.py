67a0b8eddf8f4a774b59193dd512b3518ef91cc4
Florian Vahl <florian@flova.de>
2020-08-18 17:49:37 +0200

    Fake Vision: Switch to goal posts

--- humanoid_league_gazebo_world/scripts/fake_vision.py
+++ humanoid_league_gazebo_world/scripts/fake_vision.py
@@ -8,7 +8,6 @@ from sensor_msgs.msg import CameraInfo
 from tf2_geometry_msgs import PoseStamped
 from gazebo_msgs.msg import ModelStates, ModelState
 from humanoid_league_msgs.msg import PoseWithCertainty, PoseWithCertaintyArray
-from humanoid_league_msgs.msg import GoalRelative
 
 rospy.init_node("fake_vis_in_sim")
 
@@ -17,20 +16,20 @@ tf_listener = tf2_ros.TransformListener(tf_buffer)
 
 
 ball_pose = None
-goal1_pose = None
-goal2_pose = None
+goal_post_1_pose = None
+goal_post_2_pose = None
 cam_info = None
 ball_pub = None
-goal_pub = None
+goal_posts_pub = None
 
 def state_update(state_msg):
     global ball_pose
-    global goal1_pose
-    global goal2_pose
+    global goal_post_1_pose
+    global goal_post_2_pose
     global tf_buffer
     global cam_info
     global ball_pub
-    global goal_pub
+    global goal_posts_pub
 
     if not cam_info:
         return
@@ -41,21 +40,19 @@ def state_update(state_msg):
             ball_indices.append(i)
 
 
-    goal_index1 = 0
-    goal_index2 = 0
+    post_index1 = 0
+    post_index2 = 0
     for name in state_msg.name:
         if name == "teensize_goal":
-            if goal_index1 == 0:
-                goal_index1 = goal_index2
+            if post_index1 == 0:
+                post_index1 = post_index2
             else:
                 break
-        goal_index2 += 1
+        post_index2 += 1
 
     ball_msgs = []
     for ball_index in ball_indices:
         ball_pose = state_msg.pose[ball_index]
-        goal1_pose = state_msg.pose[goal_index1]
-        goal2_pose = state_msg.pose[goal_index2]
 
         ball_pose_stamped = PoseStamped()
         ball_pose_stamped.header.stamp = rospy.Time.now()
@@ -88,23 +85,19 @@ def state_update(state_msg):
             balls_relative_msg.poses = ball_msgs
             ball_pub.publish(balls_relative_msg)
 
+    goal = PoseWithCertaintyArray()
 
-    for gp in (goal1_pose, goal2_pose):
-        goal_pose_stamped = PoseStamped()
-        goal_pose_stamped.header.stamp = rospy.Time.now()
-        goal_pose_stamped.header.frame_id = "map"
-        goal_pose_stamped.pose = gp
+    goal_post_1_pose = state_msg.pose[post_index1]
+    goal_post_2_pose = state_msg.pose[post_index2]
 
-        goal = GoalRelative()
-        goal.header.stamp = goal_pose_stamped.header.stamp
-        goal.header.frame_id = "base_footprint"
+    for gp in (goal_post_1_pose, goal_post_2_pose):
+        goal_post_stamped = PoseWithCertainty()
+        goal_post_stamped.pose.pose = gp
 
-        left_post = deepcopy(goal_pose_stamped)
-        left_post.pose.position.y += 1.35
-        left_post_in_camera_optical_frame = tf_buffer.transform(left_post, cam_info.header.frame_id, timeout=rospy.Duration(0.5))
+        left_post = deepcopy(goal_post_stamped)
+        left_post.pose.pose.position.y += 1.35
 
-        lpost_visible = False
-        rpost_visible = False
+        left_post_in_camera_optical_frame = tf_buffer.transform(left_post.pose, cam_info.header.frame_id, timeout=rospy.Duration(0.5))
 
         if left_post_in_camera_optical_frame.pose.position.z >= 0:
             p = [left_post_in_camera_optical_frame.pose.position.x, left_post_in_camera_optical_frame.pose.position.y, left_post_in_camera_optical_frame.pose.position.z]
@@ -113,11 +106,13 @@ def state_update(state_msg):
             p_pixel = p_pixel * (1 / p_pixel[2])
 
             if 0 < p_pixel[0] <= cam_info.width and 0 < p_pixel[1] <= cam_info.height:
-                goal.left_post = tf_buffer.transform(left_post, "base_footprint",
+                left_post = PoseWithCertainty()
+                left_post.confidence = 1
+                left_post.pose = tf_buffer.transform(left_post.pose, "base_footprint",
                                                     timeout=rospy.Duration(0.5)).pose.position
-                lpost_visible = True
+                goal.poses.append(left_post)
 
-        right_post = goal_pose_stamped
+        right_post = goal_post_stamped
         right_post.pose.position.y -= 1.35
         right_post_in_camera_optical_frame = tf_buffer.transform(right_post, cam_info.header.frame_id, timeout=rospy.Duration(0.5))
         if right_post_in_camera_optical_frame.pose.position.z >= 0:
@@ -127,17 +122,14 @@ def state_update(state_msg):
             p_pixel = p_pixel * (1 / p_pixel[2])
 
             if 0 < p_pixel[0] <= cam_info.width and 0 < p_pixel[1] <= cam_info.height:
-                goal.right_post = tf_buffer.transform(right_post_in_camera_optical_frame, "base_footprint",
+                right_post = PoseWithCertainty()
+                right_post.confidence = 1
+                right_post.pose = tf_buffer.transform(right_post_in_camera_optical_frame, "base_footprint",
                                                     timeout=rospy.Duration(0.5)).pose.position
-                rpost_visible = True
+                goal.poses.append(right_post)
 
-        if rpost_visible or lpost_visible:
-            if not lpost_visible:
-                goal.left_post = goal.right_post
-            elif not rpost_visible:
-                goal.right_post = goal.left_post
-            goal.confidence = 1
-            goal_pub.publish(goal)
+        if len(goal.poses) > 0:
+            goal_posts_pub.publish(goal)
 
 
 def cam_info_cb(msg):
@@ -151,7 +143,7 @@ if __name__ == "__main__":
     tf_buffer.can_transform("map", "camera_optical_frame", rospy.Time(0), timeout=rospy.Duration(30))
 
     balls_pub = rospy.Publisher("/balls_relative", PoseWithCertaintyArray, queue_size=1)
-    goal_pub = rospy.Publisher("/goal_relative", GoalRelative, queue_size=1)
+    goal_posts_pub = rospy.Publisher("/goal_posts_in_image", PoseWithCertaintyArray, queue_size=1)
     model_subscriber = rospy.Subscriber("/gazebo/model_states", ModelStates, state_update, tcp_nodelay=True, queue_size=1)
     cam_info_sub = rospy.Subscriber("/camera_info", CameraInfo, cam_info_cb)
 
